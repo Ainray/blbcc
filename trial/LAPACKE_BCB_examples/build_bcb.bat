69a4be5fdfa72b8e9c3a7896b1898e7048837692
ainray <wwzhang0421@163.com>
2019-10-03 18:16:47 +0800

    rad version of cblas is added. Reamde file is changed

--- trial/LAPACKE_BCB_examples/build_bcb.bat
+++ trial/LAPACKE_BCB_examples/build_bcb.bat
@@ -1,6 +1,6 @@
 mkdir build_bcb
 cd build_bcb
 cmake -DCMAKE_C_COMPILER=bcc32.exe -DCMAKE_CXX_COMPILER=bcc32.exe -G Ninja ..
-ninja -v
-.\release\example_DGESV_rowmajor
+ninja
+.\release\ore
 pause "Enter any Key..."
